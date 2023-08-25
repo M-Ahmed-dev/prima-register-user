9ac75675e312b6d5d27b5683c51cd03557271352
M-Ahmed-dev <winterfell551@gmail.com>
2023-08-25 18:34:23 +0500

    conitionally rendering components based on user status

--- src/components/InformationForm/InformationForm.jsx
+++ src/components/InformationForm/InformationForm.jsx
@@ -7,6 +7,7 @@ import {
   Text,
   useTheme,
 } from "@chakra-ui/react";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const InformationForm = ({ data }) => {
@@ -62,6 +63,38 @@ const InformationForm = ({ data }) => {
 
       return null;
     });
+  };
+
+  //
+
+  const handleSubmit = async () => {
+    const endpoint =
+      "https://maxenius.agency/staging/obetus/wp-json/priima_user/create?key=eyJvcmRlcl9pZCI6ODk4NSwiaXRlbV9pZCI6MzIsInZfaWQiOjEwMzgsImVtYWlsIjoiMnNhbXBsZUBibGsuY2MiLCJlbmRwb2ludCI6Imh0dHBzOlwvXC9tYXhlbml1cy5hZ2VuY3lcL3N0YWdpbmdcL29iZXR1c1wvd3AtanNvblwvcHJpaW1hX3VzZXJcLyJ9";
+
+    const requestData = {
+      fields_data: {
+        basic: {
+          email: data.basic.find((item) => item.label === "User Email").value,
+          first_name: data.basic.find((item) => item.label === "User Name")
+            .value,
+          phone: data.basic.find((item) => item.label === "User Phone").value,
+        },
+        additional: {
+          // Populate additional fields here
+          // Example:
+          // "67453": "samsdfk ",
+          // "41522": "sdfg sfxcc",
+          // "26288": "sdfr sfxcc"
+        },
+      },
+    };
+
+    try {
+      const response = await axios.post(endpoint, requestData);
+      console.log("Response:", response.data); // You can handle the response here
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
